f7929cf247d2b432b7918375516ca571cf5cabc3
Martinekk <martinmatostanogmail.com>
2022-10-06 08:29:58 +0200

    First commit

---
+++ src/main/java/FakeTrelloBackEnd/example/FakeTrelloBackEnd/FakeTrelloBackEndApplication.java
@@ -0,0 +1,13 @@
+package FakeTrelloBackEnd.example.FakeTrelloBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeTrelloBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeTrelloBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FakeTrelloBackEnd/example/FakeTrelloBackEnd/FakeTrelloBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package FakeTrelloBackEnd.example.FakeTrelloBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeTrelloBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
