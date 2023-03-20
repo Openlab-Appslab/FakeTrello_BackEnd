38c435573985f26ebd685816468948324e37f50b
Martinekk <martinmatostano@gmail.com>
2023-03-20 20:28:00 +0100

    add enum for state

--- src/main/java/FakeTrelloBackEnd/example/FakeTrelloBackEnd/bussiness/enums/TaskState.java
+++ src/main/java/FakeTrelloBackEnd/example/FakeTrelloBackEnd/bussiness/enums/TaskState.java
@@ -1,7 +1,22 @@
 package FakeTrelloBackEnd.example.FakeTrelloBackEnd.bussiness.enums;
 
 public enum TaskState {
-    TODO,
-    INPROGRESS,
-    DONE
+    TODO{
+        @Override
+        public String toString() {
+            return "toDo";
+        }
+    },
+    INPROGRESS{
+        @Override
+        public String toString() {
+            return "inprogress";
+        }
+    },
+    DONE{
+        @Override
+        public String toString() {
+            return "done";
+        }
+    }
 }
