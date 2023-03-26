4c5ee47634f515a27a141bfc9752ce400bf4f351
Martinekk <martinmatostano@gmail.com>
2023-03-26 15:00:49 +0200

    change exception

--- src/main/java/FakeTrelloBackEnd/example/FakeTrelloBackEnd/exception/userException/UserAlreadyExists.java
+++ src/main/java/FakeTrelloBackEnd/example/FakeTrelloBackEnd/exception/userException/UserAlreadyExists.java
@@ -3,7 +3,7 @@ package FakeTrelloBackEnd.example.FakeTrelloBackEnd.exception.userException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.IM_USED)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class UserAlreadyExists extends RuntimeException{
     public UserAlreadyExists(String message){super(message);}
 }
